1fcc137756ae98eb1d355b639666fa19431f8930
jhonatanvicg <jhonatanvicg@gmail.com>
2022-04-27 17:48:52 -0500

    Manejo de errores a la hora de listar los posts

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import axios from "../hooks/useAxios"
 import request from "../hooks/useRequest";
 import { createClient } from 'pexels';
 const query = ['Nature','Dogs','Cats','Animals','People','Sea','Funny'];
-import { useEffect,useState } from "react";
+import { useEffect,useState, useRef, ReactDOM } from "react";
 
 const HomePage = ()=>{
     
@@ -11,35 +11,91 @@ const HomePage = ()=>{
     const client = createClient(API_KEY);
     const [posts,setPosts] = useState('')
     const [urlVideos, setUrlVideos] = useState('')
+    const targetObserver = useRef();
+    const containerPosts = useRef();
+    const [numberPage, setNumberPage] = useState(1)
+    const intersectionOptions = {
+        threshold:1
+    }
+
+
+    const onIntersect = ([entry])=>{
+        
+        if(entry.isIntersecting){
+            getData()
+
+        }
+    }
+
+
+    async function getData(){
+        const answer = await axios.get(request.fetchPosts)
+        setPosts(answer.data.data)
+        
+        let genre = query[Math.floor(Math.random() * 8 )]
+        client.videos.search({query:genre,page:numberPage}).then(response => {
+            let tempArrVideos = [];
+            response.videos.forEach((el)=>{
+                let randomVideoValue = Math.floor(Math.random()* 16)
+                if( response.videos[randomVideoValue] != undefined){
+                    tempArrVideos.push(response.videos[randomVideoValue].video_files[0].link)
+                }
+            })
+            setUrlVideos(tempArrVideos)
+            //cleanUp(response.next_page)
+            //renderItems()
+
+        });
+
+    }
+   
+    
+    const cleanUp = (nextPage)=>{
+        if(nextPage!=null){
+            setNumberPage(numberPage + 1)
+        }
+    }
+    
+    const renderItems = ()=>{
+        try {
+            posts.forEach((item,index)=>{
+                containerPosts.current.appendChild(createItem(item,index))
+            })
+        } catch (error) {
+            console.log(error)
+        }
+
+        
+    }
+
+    const createItem = (item,index) =>{
+        return  (
+                <Post
+                    key={index}
+                    videoLink={urlVideos[index]}
+                    pageName={item.pageName} 
+                    comments={item.comments} 
+                    date={item.date} 
+                    description={item.description} 
+                    share={item.share} 
+                    likes={item.likes}  
+                />
+        )
+    }
+    
     useEffect(()=>{
-         async function getData(){
-            const answer = await axios.get(request.fetchPosts)
-            setPosts(answer.data.data)
-            
-            let genre = query[Math.floor(Math.random() * 8 )]
-            console.log("este es ek gn}enero:",genre)
-            client.videos.search({query:genre}).then(response => {
-                let tempArrVideos = [];
-                console.log(response)
-                response.videos.forEach((el)=>{
-                    let randomVideoValue = Math.floor(Math.random()* 16)
-                    if( response.videos[randomVideoValue] != undefined){
-                        console.log(response.videos[Math.floor(Math.random()* 16)])
-                        tempArrVideos.push(response.videos[randomVideoValue].video_files[0].link)
-                    }
-                })
-                setUrlVideos(tempArrVideos)
-            });
+        if(window !=undefined){
+            //let observer = new IntersectionObserver(onIntersect, intersectionOptions)
+            //observer.observe(targetObserver.current)
+
         }
         getData()
-
-
 
     },[])
 
     return <>
         {
-            posts == '' || urlVideos == '' ? null || urlVideos== undefined :
+            posts == '' || urlVideos == '' || urlVideos== undefined ? null  :
             posts.map((page,index)=>(
                 <Post
                     key={index}
@@ -52,7 +108,13 @@ const HomePage = ()=>{
                     likes={page.likes}  
                 />
             ))
+
+            //<div ref={containerPosts} className="containerPosts">
+
+          //  </div>
+        //<div ref={targetObserver} className="targetObserver"></div>
         }
+        
     </>
 }
 
